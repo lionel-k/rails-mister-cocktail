6320803c6ad7899cf0c0de2378a0e7292c250461
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-23 17:55:10 +0100

    re seeding the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,38 @@
 require 'json'
 require 'open-uri'
 
-Ingredient.delete_all
-Cocktail.delete_all
+# Ingredient.delete_all
+# Cocktail.delete_all
 
 
-puts 'fetching list of ingredients'
+# puts 'fetching list of ingredients'
 
-url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients = JSON.parse(open(url).read)
-drinks = ingredients["drinks"]
+# url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredients = JSON.parse(open(url).read)
+# drinks = ingredients["drinks"]
 
-puts "Creating ingredients"
+# puts "Creating ingredients"
 
-drinks.each do |hash_ingredient|
-  # |hash_ingredient| hash_ingredient["strIngredient1"]
-  # p hash_ingredient["strIngredient1"]
-  Ingredient.create(name: hash_ingredient["strIngredient1"])
-end
+# drinks.each do |hash_ingredient|
+#   Ingredient.create(name: hash_ingredient["strIngredient1"])
+# end
 
-puts "Finishing creating ingredients in the db"
+# puts "Finishing creating ingredients in the db"
 
 puts 'fetching list of cocktails'
 
 url = 'https://raw.githubusercontent.com/teijo/iba-cocktails/master/recipes.json'
 cocktails = JSON.parse(open(url).read)
 
-puts "Creating cocktails"
+# puts cocktails
 
+# puts "Creating cocktails"
+ingredients = []
 cocktails.each do |cocktail|
-  Cocktail.create(name: cocktail["name"])
+  cocktail["ingredients"].each do |details_ingredient|
+    ingredients << details_ingredient["ingredient"]
+  end
 end
 
-puts "Finishing creating cocktails in the db"
+ingredients.delete(nil)
+p ingredients
