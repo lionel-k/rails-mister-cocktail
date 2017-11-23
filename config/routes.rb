479d0baa35bfef8ef4d4628a5966e500364663ef
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-23 17:06:56 +0100

    home page with bootstrap done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [:new, :index, :show, :create] do
-    resources :doses, only: [:create]
+    resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
   root to: "cocktails#index"
