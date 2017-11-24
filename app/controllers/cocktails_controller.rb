bd517604fb5ff79ae3bc2fbbc917e7b288416f1a
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-24 17:57:56 +0100

    adding feature for adding the preparation when adding a new cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -29,6 +29,6 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :photo)
+    params.require(:cocktail).permit(:name, :photo, :preparation)
   end
 end
